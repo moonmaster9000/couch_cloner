a110721dd8789cab94e1ed25bed63aa9e2bfecde
Matt Parker <moonmaster9000@gmail.com>
2011-07-27 14:46:20 -0400

    refactored out query map/reduce and methods into Query module

--- lib/couch_cloner/couch_cloner.rb
+++ lib/couch_cloner/couch_cloner.rb
@@ -2,5 +2,6 @@ module CouchCloner
   def self.included(base)
     base.send :include, CouchCloner::Clone
     base.send :include, CouchCloner::Scheduling
+    base.send :include, CouchCloner::Query
   end
 end
