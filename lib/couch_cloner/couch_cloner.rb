77e0bc991796e01b1bf1373178855e54e9c779fb
Matt Parker <moonmaster9000@gmail.com>
2011-09-15 15:20:34 -0400

    bug fix: only include CouchCloner::CouchPublish or CouchCloner::CouchVisible if CouchPublish/CouchVisible is an ancestor

--- lib/couch_cloner/couch_cloner.rb
+++ lib/couch_cloner/couch_cloner.rb
@@ -5,11 +5,11 @@ module CouchCloner
     base.send :include, CouchCloner::Scheduling
     base.send :include, CouchCloner::Query
     
-    if defined? ::CouchPublish && base.ancestors.include?(::CouchPublish)
+    if defined?(::CouchPublish) && base.ancestors.include?(::CouchPublish)
       base.send :include, CouchCloner::CouchPublish
     end
 
-    if defined? ::CouchVisible && base.ancestors.include?(::CouchVisible)
+    if defined?(::CouchVisible) && base.ancestors.include?(::CouchVisible)
       base.send :include, CouchCloner::CouchVisible
     end
   end
